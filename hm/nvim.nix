216f888f58cf2bcc637068fb4d7ea2a13f2d73ad
Gin <divyang_2k10@yahoo.com>
2022-02-09 00:21:34 +0530

    Cleanup

--- hm/nvim.nix
+++ hm/nvim.nix
@@ -11,13 +11,17 @@
     };
 
     plugins = {
+
+      nix.enable = true;
+
       lsp = {
         enable = true;
-	servers = {
-	  rust-analyzer.enable = true;
-	  rnix-lsp.enable = true;
-	};
+	      servers = {
+	        rust-analyzer.enable = true;
+	        rnix-lsp.enable = true;
+	      };
       };
+
     };
 
     extraPlugins = with pkgs.vimPlugins; [
@@ -25,8 +29,23 @@
     ];
 
     options = {
+      # Indentation
       autoindent = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+      backspace = "indent,eol,start";
+
+      # Text
+      showmatch = true;
+      mouse = "a";
+      number = true;
+      relativenumber = true;
+      ttyfast = true;
       clipboard = "unnamedplus";
+      ls = 0;
+      statusline = "%#StatuslineNC#\ %n\ %#Statusline#\ %#LineNr#\ %f\ %m\ %r\ %=\ %y\ %#StatuslineNC#\ %l:%c\ %p%%";
+      # statusline = "%#StatuslineNC#\ %n\ %#Statusline#\ %#LineNr#\ %f\ %m\ %r\ %=\ %y\ %#StatuslineNC#\ %l:%c\ %p%%\";
     };
 
   };
