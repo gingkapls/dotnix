2b4e55c4a39f71e255bfbdd7ae61a63ac5351d90
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-04 00:25:02 +0530

    vscode: disable userSettings

--- hm/programs/vscode/default.nix
+++ hm/programs/vscode/default.nix
@@ -1,8 +1,9 @@
-{ config, pkgs, lib, nix-vscode-extensions, ... }:
+{ config, osConfig, pkgs, lib, nix-vscode-extensions, ... }:
 
 with lib;
 let 
   cfg = config.modules.programs.vscode;
+  font-mono = builtins.head osConfig.fonts.fontconfig.defaultFonts.monospace;
 in
 {
   options.modules.programs.vscode = {
@@ -25,21 +26,29 @@ in
         wallabyjs.console-ninja
       ];
 
-      userSettings = {
-        "editor.fontLigatures" = false;
-        "editor.formatOnSave" = true;
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "editor.cursorSmoothCaretAnimation" = "explicit";
-        "console-ninja.featureSet" = "Community";
+      # userSettings = {
+        # "editor.fontLigatures" = false;
+        # "editor.formatOnSave" = true;
+        # "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        # "editor.fontFamily" = "${font-mono}";
 
-        "window.titleBarStyle" = "custom";
-        "window.zoomLevel" = 1.5;
-        "terminal.integrated.env.linux" = {};
+        # "console-ninja.featureSet" = "Community";
 
-        "extensions.experimental.affinity" = {
-          "asvetliakov.vscode-neovim" = 1;
-        };
-      };
+        # "window.titleBarStyle" = "custom";
+        # "window.zoomLevel" = 1.5;
+        # "terminal.integrated.env.linux" = {};
+
+        # # Themeing
+        # # "window.autoDetectColorScheme" = true;
+        # "workbench.colorTheme" = "Rosé Pine Moon";
+        # # "workbench.preferredLightColorTheme" = "Rosé Pine Dawn";
+        # # "workbench.preferredDarkColorTheme" = "Rosé Pine Moon";
+
+
+        # "extensions.experimental.affinity" = {
+          # "asvetliakov.vscode-neovim" = 1;
+        # };
+      # };
     };
   };
 
