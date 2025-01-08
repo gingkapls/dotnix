7af17eecc284fe394c09843d9022f5ca3b7e0487
gin <73906888+gingkapls@users.noreply.github.com>
2025-01-08 14:05:15 +0530

    feat!: disable fish custom colors

--- hm/shells/fish.nix
+++ hm/shells/fish.nix
@@ -30,25 +30,25 @@ in {
         set -g fish_greeting
         fish_vi_key_bindings 
 
-        set fish_color_normal '#${base05}'
-        set fish_color_command '#${base0D}' --bold
-        set fish_color_quote '#${base0B}'
-        set fish_color_redirection '#${base0D}'
-        set fish_color_end '#${base05}'
-        set fish_color_error '#${base08}'
-        set fish_color_param '#${base0A}'
-        set fish_color_comment '#${base03}'
-        set fish_color_match '#${base05}'
-        set fish_color_selection '#${base03}'
-        set fish_color_search_match '#${base0A}' --bold
-        set fish_color_operator '#${base0B}'
-        set fish_color_escape '#${base0C}'
-        set fish_color_cwd '#${base0D}' --bold
-        set fish_color_autosuggestion '#${base03}'
-        set fish_color_user '#${base0E}'
-        set fish_color_host '#${base0D}'
-        set fish_color_host_remote '#${base0A}'
-        set fish_color_cancel '#${base08}'
+        # set fish_color_normal '#${base05}'
+        # set fish_color_command '#${base0D}' --bold
+        # set fish_color_quote '#${base0B}'
+        # set fish_color_redirection '#${base0D}'
+        # set fish_color_end '#${base05}'
+        # set fish_color_error '#${base08}'
+        # set fish_color_param '#${base0A}'
+        # set fish_color_comment '#${base03}'
+        # set fish_color_match '#${base05}'
+        # set fish_color_selection '#${base03}'
+        # set fish_color_search_match '#${base0A}' --bold
+        # set fish_color_operator '#${base0B}'
+        # set fish_color_escape '#${base0C}'
+        # set fish_color_cwd '#${base0D}' --bold
+        # set fish_color_autosuggestion '#${base03}'
+        # set fish_color_user '#${base0E}'
+        # set fish_color_host '#${base0D}'
+        # set fish_color_host_remote '#${base0A}'
+        # set fish_color_cancel '#${base08}'
         
         # Pager colors
         set fish_pager_color_background '#${base01}'
