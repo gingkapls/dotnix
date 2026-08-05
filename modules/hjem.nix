f152f4d223577913b7195944965a5db1b703d954
gin <73906888+gingkapls@users.noreply.github.com>
2026-08-05 15:40:14 +0530

    feat: refactor modules

---
+++ modules/hjem.nix
@@ -0,0 +1,54 @@
+{ ... }: {
+  hjem.users.gin = {
+      user = "gin";
+      directory = "/home/gin";
+      clobberFiles = true;
+  };
+
+  users.user.gin.packages = with pkgs; [
+    # Utilities
+    coreutils tree jq rename gh
+    krita inkscape
+    droidcam cheese guvcview
+    imagemagick imv gcolor3 amberol 
+    playerctl pamixer pavucontrol
+    networkmanagerapplet
+    aria2 rclone yt-dlp
+    scrcpy
+    inotify-tools rmlint lm_sensors p7zip comma
+    glib gsettings-desktop-schemas
+    hyperfine
+    gammastep
+    localsend
+    pciutils usbutils
+
+    # Applications
+    google-chrome
+    gnome-network-displays
+    qbittorrent transmission_4-gtk
+    telegram-desktop obsidian
+    zathura foliate calibre
+    pear-desktop
+    lutris mangohud
+    mpv
+    kitty
+    wezterm
+    blackbox-terminal
+    styluslabs-write-bin
+    xournalpp
+    anki
+    nautilus
+    bottles
+    faugus-launcher
+    protontricks
+    libreoffice
+
+    wl-clipboard
+    fuzzel
+    wf-recorder 
+    xwayland-satellite
+    noctalia
+    firefox
+  ];
+
+}
