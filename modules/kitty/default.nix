f965f02390ec02c3531e3495a7e8c2bc9f51bf47
gin <73906888+gingkapls@users.noreply.github.com>
2026-08-05 20:17:07 +0530

    fix(kitty): add package

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -1,7 +1,12 @@
-{ ...}:
+{ lib, pkgs, ... }:
 
 {
   hjem.users.gin.xdg.config.files = {
     "kitty/kitty.conf".source = ./kitty.conf;
+  };
+
+  users.users.gin.packages = lib.attrValues {
+    inherit (pkgs)
+    kitty;
   };
 }
